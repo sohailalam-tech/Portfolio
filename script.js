3303358e0b20ec92fa9fd34fa10c1c4f271229bb
sohailalam-tech <atifalam298@gmail.com>
2025-03-12 13:54:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector("#menu");
+const nav = document.querySelector(".links");
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
